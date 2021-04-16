12d97549dedabdb52761fcc5b41e511174f4b8b8
Mahdi Ghaharian <m.ghaharian@gmail.com>
2021-04-16 18:31:34 +0430

    first commit to initialize the project

---
+++ src/main/java/ir/mghhrn/ttbackend/TtbackendApplication.java
@@ -0,0 +1,13 @@
+package ir.mghhrn.ttbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/mghhrn/ttbackend/TtbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mghhrn.ttbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
